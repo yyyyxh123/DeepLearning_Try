c66051b844e6283cb4c289b37910d14ad6ae13d0
Xinhe Yang <ieyangxh1213@gmail.com>
2025-07-28 16:07:01 +1000

    complete chapter 4_3

--- d2l_tutorial/chapter4_3.py
+++ d2l_tutorial/chapter4_3.py
@@ -5,11 +5,19 @@ from d2l import torch as d2l
 class Classifier(d2l.Module):  #@save
     """The base class of classification models."""
     def validation_step(self, batch):
-        Y_hat = self(*batch[:-1])
+        Y_hat = self(*batch[:-1]) #batch is a tuple batch[0] is future [1] is label
         self.plot('loss', self.loss(Y_hat, batch[-1]), train=False)
         self.plot('acc', self.accuracy(Y_hat, batch[-1]), train=False)
 # %%
 @d2l.add_to_class(d2l.Module)  #@save
 def configure_optimizers(self):
     return torch.optim.SGD(self.parameters(), lr=self.lr)
+# %%
+@d2l.add_to_class(Classifier)  #@save
+def accuracy(self, Y_hat, Y, averaged=True):
+    """Compute the number of correct predictions."""
+    Y_hat = Y_hat.reshape((-1, Y_hat.shape[-1]))
+    preds = Y_hat.argmax(axis=1).type(Y.dtype)
+    compare = (preds == Y.reshape(-1)).type(torch.float32)
+    return compare.mean() if averaged else compare
 # %%
