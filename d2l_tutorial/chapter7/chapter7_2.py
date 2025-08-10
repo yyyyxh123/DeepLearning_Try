2b11636ee74062c689253b4a210de836985efb7d
Xinhe Yang <ieyangxh1213@gmail.com>
2025-08-10 16:09:41 +1000

    The background of CNN

---
+++ d2l_tutorial/chapter7/chapter7_2.py
@@ -0,0 +1,61 @@
+# %%
+import torch
+from torch import nn
+from d2l import torch as d2l
+# %%
+def corr2d(X, K):  #@save
+    """Compute 2D cross-correlation."""
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i:i + h, j:j + w] * K).sum()
+    return Y
+# %%
+X = torch.tensor([[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]])
+K = torch.tensor([[0.0, 1.0], [2.0, 3.0]])
+corr2d(X, K)
+# %%
+class Conv2D(nn.Module):
+    def __init__(self, kernel_size):
+        super().__init__()
+        self.weight = nn.Parameter(torch.rand(kernel_size))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        return corr2d(x, self.weight) + self.bias
+# %%
+X = torch.ones((6, 8))
+X[:, 2:6] = 0
+X
+# %%
+K = torch.tensor([[1.0, -1.0]])
+# %%
+Y = corr2d(X, K)
+Y
+# %%
+corr2d(X.t(), K)
+# %%
+# Construct a two-dimensional convolutional layer with 1 output channel and a
+# kernel of shape (1, 2). For the sake of simplicity, we ignore the bias here
+conv2d = nn.LazyConv2d(1, kernel_size=(1, 2), bias=False)
+
+# The two-dimensional convolutional layer uses four-dimensional input and
+# output in the format of (example, channel, height, width), where the batch
+# size (number of examples in the batch) and the number of channels are both 1
+X = X.reshape((1, 1, 6, 8))
+Y = Y.reshape((1, 1, 6, 7))
+lr = 3e-2  # Learning rate
+
+for i in range(10):
+    Y_hat = conv2d(X)
+    l = (Y_hat - Y) ** 2
+    conv2d.zero_grad()
+    l.sum().backward()
+    # Update the kernel
+    conv2d.weight.data[:] -= lr * conv2d.weight.grad
+    if (i + 1) % 2 == 0:
+        print(f'epoch {i + 1}, loss {l.sum():.3f}')
+# %%
+conv2d.weight.data.reshape((1, 2))
+# %%
---
+++ d2l_tutorial/chapter7/chapter7_3.py
@@ -0,0 +1,31 @@
+# %%
+import torch
+from torch import nn
+# %%
+# We define a helper function to calculate convolutions. It initializes the
+# convolutional layer weights and performs corresponding dimensionality
+# elevations and reductions on the input and output
+def comp_conv2d(conv2d, X):
+    # (1, 1) indicates that batch size and the number of channels are both 1
+    X = X.reshape((1, 1) + X.shape)
+    Y = conv2d(X)
+    # Strip the first two dimensions: examples and channels
+    return Y.reshape(Y.shape[2:])
+
+# 1 row and column is padded on either side, so a total of 2 rows or columns
+# are added
+conv2d = nn.LazyConv2d(1, kernel_size=3, padding=1)
+X = torch.rand(size=(8, 8))
+comp_conv2d(conv2d, X).shape
+# %%
+# We use a convolution kernel with height 5 and width 3. The padding on either
+# side of the height and width are 2 and 1, respectively
+conv2d = nn.LazyConv2d(1, kernel_size=(5, 3), padding=(2, 1))
+comp_conv2d(conv2d, X).shape
+# %%
+conv2d = nn.LazyConv2d(1, kernel_size=3, padding=1, stride=2)
+comp_conv2d(conv2d, X).shape
+# %%
+conv2d = nn.LazyConv2d(1, kernel_size=(3, 5), padding=(0, 1), stride=(3, 4))
+comp_conv2d(conv2d, X).shape
+# %%
