58e5bb5bf9d9ce30a6c94ee0c60c399cf5be59b0
Xinhe Yang <ieyangxh1213@gmail.com>
2025-08-18 16:52:33 +1000

    part of chapter12_6

--- d2l_tutorial/chapter12/chapter12_6.py
+++ d2l_tutorial/chapter12/chapter12_6.py
@@ -9,3 +9,15 @@ def gd_2d(x1, x2, s1, s2):
     return (x1 - eta * 0.2 * x1, x2 - eta * 4 * x2, 0, 0)
 
 d2l.show_trace_2d(f_2d, d2l.train_2d(gd_2d))
+# %%
+eta = 0.6
+d2l.show_trace_2d(f_2d, d2l.train_2d(gd_2d))
+# %%
+def momentum_2d(x1, x2, v1, v2):
+    v1 = beta * v1 + 0.2 * x1
+    v2 = beta * v2 + 4 * x2
+    return x1 - eta * v1, x2 - eta * v2, v1, v2
+
+eta, beta = 0.6, 0.5
+d2l.show_trace_2d(f_2d, d2l.train_2d(momentum_2d))
+# %%
