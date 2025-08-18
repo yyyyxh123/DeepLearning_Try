3f563e7f285aef6e34c608420758768ff8c1bc19
Xinhe Yang <ieyangxh1213@gmail.com>
2025-08-18 15:08:42 +1000

    some part of chapter12_6

---
+++ d2l_tutorial/chapter12/chapter12_6.py
@@ -0,0 +1,11 @@
+# %%
+import torch
+from d2l import torch as d2l
+
+eta = 0.4
+def f_2d(x1, x2):
+    return 0.1 * x1 ** 2 + 2 * x2 ** 2
+def gd_2d(x1, x2, s1, s2):
+    return (x1 - eta * 0.2 * x1, x2 - eta * 4 * x2, 0, 0)
+
+d2l.show_trace_2d(f_2d, d2l.train_2d(gd_2d))
